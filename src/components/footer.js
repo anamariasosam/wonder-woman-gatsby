97fbb3da3b9828edd53486b2febe80cb202c9f21
Ana María <anamariasosam@gmail.com>
2018-08-10 23:13:20 -0500

    add card, footer and header components

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Footer = ({ siteTitle }) => (
+  <div className="hero-foot">
+    <div className="container">
+      <div className="tabs is-centered">
+        @anamariasosam
+      </div>
+    </div>
+  </div>
+)
+
+export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,33 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <div className="hero-head">
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="../">
+            { siteTitle }
+          </a>
+          <span className="navbar-burger burger" data-target="navbarMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenu" className="navbar-menu">
+          <div className="navbar-end">
+            <div className="tabs is-right">
+              <ul>
+                <li><Link to="/">Inicio</Link></li>
+                <li>
+                  <Link to="/features/">Características</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   </div>
 )
 
