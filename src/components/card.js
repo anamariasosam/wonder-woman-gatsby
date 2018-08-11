8d8ed743e7da10b0295a631aa434e62a0bb2451b
Ana María <anamariasosam@gmail.com>
2018-08-11 09:28:15 -0500

    remove footer and use props on card

--- src/components/card.js
+++ src/components/card.js
@@ -2,18 +2,18 @@ import React from 'react'
 import Link from 'gatsby-link'
 import '../styles/card.scss'
 
-const Card = ({ siteTitle }) => (
+const Card = ({ feature: { title, description, imageUrl } }) => (
   <div className="column is-4">
     <div className="card is-shady">
       <div className="card-image">
-        <figure className="image is-4by3">
-          <img src="https://source.unsplash.com/RWnpyGtY1aU" alt="Placeholder image" className="modal-button" data-target="modal-image2" />
+        <figure className="image is-5by3">
+          <img src={imageUrl} alt="Placeholder image" className="modal-button" data-target="modal-image2" />
         </figure>
       </div>
       <div className="card-content">
         <div className="content">
-          <h4>Click on image above</h4>
-          <p>Purus semper eget duis at tellus at urna condimentum mattis. Non blandit massa enim nec. Integer enim neque volutpat ac tincidunt vitae semper quis. Accumsan tortor posuere ac ut consequat semper viverra nam.</p>
+          <h4>{title}</h4>
+          <p>{description}</p>
         </div>
       </div>
     </div>
