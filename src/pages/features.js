5da6c4240e9029ae45278467ebba5157ea43389f
Ana María <anamariasosam@gmail.com>
2018-08-10 23:13:39 -0500

    add bulma and create new pages

---
+++ src/pages/features.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import Card from '../components/card.js'
+
+const Features = () => (
+  <section className="container">
+    <div className="columns features">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </section>
+)
+
+export default Features
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,31 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  <section className="hero is-fullheight is-default is-bold">
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <div className="columns is-vcentered">
+          <div className="column is-5">
+            <figure className="image is-4by3">
+              <img src="https://static01.nyt.com/images/2017/06/02/arts/02WONDER/02WONDER-master1050.jpg" alt="Mujer Maravilla" />
+            </figure>
+          </div>
+          <div className="column is-6 is-offset-1">
+            <h1 className="title is-2">
+              Mujer Maravilla
+            </h1>
+            <h2 className="subtitle is-4">
+              Guerrera de las Amazonas
+            </h2>
+            <br />
+            <p className="has-text-centered">
+              <Link to="/features/" className="button is-medium is-info is-outlined">Ver más</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 )
 
 export default IndexPage
